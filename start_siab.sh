3ff5891f318e13b49601f4b949a7d81388d57e9b
root <root@ldlework.ICS.local>
2010-11-17 22:12:11 -0600

    Added link to css with font-size change and README

--- start_siab.sh
+++ start_siab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /etc/init.d/apache2 restart; 
-shellinaboxd -t --service=/:root:root:/var/www/ttybin:"python replay.py -u \${url}"
+shellinaboxd -t --service=/:root:root:/var/www/ttybin:"python replay.py -u \${url}" --css="white-on-black.css"
